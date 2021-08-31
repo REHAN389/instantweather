261f48eb822bf1f5d30b086de5fd3f2ba651e9d9
Rehan Ahmad <rehansheikh1810@gmail.com>
2021-08-31 11:39:02 +0530

    my node js first commit

---
+++ public/js/main.js
@@ -0,0 +1,49 @@
+const submitBtn = document.getElementById('submitBtn');
+const cityname = document.getElementById('cityname');
+const city_name = document.getElementById('city_name');
+const temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+const temp_no = document.getElementById('temp_no');
+const datahide = document.querySelector('.middle_layer');
+
+
+const getinfo = async (event) =>{
+    event.preventDefault();
+    let cityVal = cityname.value;
+
+   if(cityVal === ""){
+    city_name.innerText = 'Please enter city name.';
+    datahide.classList.add('data_hide');
+    
+   }else{
+       try{
+
+           let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=7b03c09da7df9387ae08fdda0ef4739d`;
+           const response = await fetch(url);
+           const data = await response.json();
+           const arrData= [data];
+           temp_real_val.innerText = arrData[0].main.temp;
+           city_name.innerText = `${arrData[0].name},${arrData[0].sys.country}`
+
+           let tempMood = arrData[0].weather[0].main;
+
+           // condition for icon of clouds sun...
+           if(tempMood == "Clear"){
+            temp_status.innerHTML = '<i class="fas fa-sun" style = "color: #eccc68;"></i>';
+           }else if (tempMood == "Clouds"){
+            temp_status.innerHTML = '<i class="fas fa-cloud" style = "color: #9daeef;"></i>';
+           }else if (tempMood == "Rain"){
+            temp_status.innerHTML = '<i class="fas fa-cloud-rain" style = "color:#a4b0be"></i>';
+           }else{
+            temp_status.innerHTML = '<i class="fas fa-cloud" style = "color: #9daeef;"></i>';
+           }
+           datahide.classList.remove('data_hide');
+
+       }catch{
+        city_name.innerText = 'Please enter city name properly.';
+        datahide.classList.add('data_hide');
+       }
+   }
+}
+
+submitBtn.addEventListener('click',getinfo);
